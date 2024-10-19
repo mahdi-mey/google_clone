31f826f0a4415ed295c2126fdcca9c847a8c9d01
Mahdi <mahdimey17@gmail.com>
2024-10-19 10:57:45 +0330

    add placeholder components for AddShortcuts and Customize components to deal with them later

---
+++ src/components/AddShortucts.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AddShortucts() {
+  return (
+    <div className='size-28 bg-gray-300'>add shortcuts...</div>
+  )
+}
---
+++ src/components/Customize.jsx
@@ -0,0 +1,8 @@
+import { BiPencil } from "react-icons/bi"; 
+import React from 'react'
+
+export default function Customize() {
+  return (
+    <button className='text-right mr-8 px-5 rounded-2xl bg-[--primary-color] text-white flex justify-end items-center gap-3'> <BiPencil /> Customize</button>
+  )
+}
