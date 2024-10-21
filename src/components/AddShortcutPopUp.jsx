e35b19d7e13eea9e270e44890d4f4043a20afd11
Mahdi <mahdimey17@gmail.com>
2024-10-21 21:28:10 +0330

    updated AddShortcutPopUp component ui and disabled styles for btn

--- src/components/AddShortcutPopUp.jsx
+++ src/components/AddShortcutPopUp.jsx
@@ -1,29 +1,74 @@
 "use client";
 
-export default function AddShortcutPopUp() {
-  function addShortcut() {}
-  function closeModal() {
-    console.log("clicked me");
+import { useState } from "react";
+
+export default function AddShortcutPopUp({ changeOpen }) {
+  const [nameInput, setNameInput] = useState("");
+  const [urlInput, setUrlnInput] = useState("");
+
+  function addShortcut(e) {
+    e.preventDefault();
   }
+
+  function closeModal() {
+    changeOpen(false);
+  }
+
+  const canSubmit = nameInput.trim().length > 0 && urlInput.trim().length > 0;
+
   return (
     <div
       className="absolute inset-0 bg-black/15 flex justify-center items-center"
-      onClick={closeModal}
+      onClick={() => closeModal}
     >
       <form
-        className="h-72 bg-white h-76 w-full max-w-[512px] mx-auto rounded-md flex flex-col justify-evenly px-3"
+        className="h-72 bg-white w-full max-w-[512px] mx-auto rounded-md flex flex-col justify-evenly px-3"
         onClick={(e) => e.stopPropagation()}
         onSubmit={addShortcut}
       >
         <h2>Add Shortcut</h2>
-        <label className="font-bold text-sm text-gray-600" htmlFor="">Name</label>
-        <input className="bg-gray-200/70 focus:border-blue-600 outline-none ps-2 rounded-t-md h-9 border-b-2 border-gray-500" type="text" name="" id="" />
-        <label className="font-bold text-sm text-gray-600" htmlFor="">Url</label>
-        <input className="bg-gray-200/70 focus:border-blue-600 outline-none ps-2 rounded-t-md h-9 border-b-2 border-gray-500" type="text" name="" id="" />
+        <label
+          className="font-bold text-sm text-gray-600"
+          htmlFor="shortcutName"
+        >
+          Name
+        </label>
+        <input
+          className="bg-gray-200/70 focus:border-blue-600 outline-none ps-2 rounded-t-md h-9 border-b-2 border-gray-500"
+          type="text"
+          name="shortcutName"
+          id="shortcutName"
+          value={nameInput}
+          onChange={(e) => setNameInput(e.target.value)}
+        />
+        <label
+          className="font-bold text-sm text-gray-600"
+          htmlFor="shortcutUrl"
+        >
+          Url
+        </label>
+        <input
+          className="bg-gray-200/70 focus:border-blue-600 outline-none ps-2 rounded-t-md h-9 border-b-2 border-gray-500"
+          type="text"
+          name="shortcutUrl"
+          id="shortcutUrl"
+          value={urlInput}
+          onChange={(e) => setUrlnInput(e.target.value)}
+        />
 
-        <div className="flex justify-end items-center">
-          <button className="py-1.5 px-2.5 rounded-full border-1 border-blue-500 text-blue-500" onClick={closeModal}>Cancel</button>
-          <button className="py-1.5 px-2.5 rounded-full ">Done</button>
+        <div className="flex justify-end items-center gap-3 mr-1">
+          <button
+            className="py-1.5 px-3 rounded-full border-1 border  border-blue-500 text-blue-500 text-base"
+            onClick={closeModal}
+          >
+            Cancel
+          </button>
+          <button
+            className="py-1.5 px-3 rounded-full disabled:cursor-not-allowed disabled:bg-gray-400 bg-blue-500 text-white text-base"
+            disabled={!canSubmit}
+          >
+            Done
+          </button>
         </div>
       </form>
     </div>
--- src/components/ShortuctsContainer.jsx
+++ src/components/ShortuctsContainer.jsx
@@ -1,25 +1,34 @@
-'use client'
+"use client";
 import { BiPlus } from "react-icons/bi";
-import {useState} from "react";
+import { useState } from "react";
 import AddShortcutPopUp from "./AddShortcutPopUp";
 
 export default function AddShortucts() {
-  const [isAddShortcutOpen, setIsAddShortcutOpen] = useState(false)
+  const [isAddShortcutOpen, setIsAddShortcutOpen] = useState(false);
   return (
-    <>
-      <div className="mt-2.5">
-        <div
-          onClick={() => setIsAddShortcutOpen(true)}
-          className="size-28 flex rounded-md cursor-pointer flex-col items-center justify-evenly hover:bg-gray-200"
-        >
-          <div className="size-12 rounded-full bg-blue-300/80 flex justify-center items-center">
-            <BiPlus className="text-lg " />
-          </div>
-          <p className="text-sm">Add shortcuts</p>
+    <div className="flex flex-row items-center justify-center gap-2 mt-2.5">
+      <a
+        href="https://sabzlearn.ir/"
+        className="size-28 flex rounded-md cursor-pointer flex-col items-center justify-evenly transition-all hover:bg-gray-200"
+      >
+        <div className="size-12 rounded-full bg-blue-300/80 flex justify-center items-center">
+          {/* Website Log */}
         </div>
+        <p className="text-sm">Example website</p>
+      </a>
+
+      <div
+        onClick={() => setIsAddShortcutOpen(true)}
+        className="size-28 flex rounded-md cursor-pointer flex-col items-center justify-evenly transition-all hover:bg-gray-200"
+      >
+        <div className="size-12 rounded-full bg-blue-300/80 flex justify-center items-center">
+          <BiPlus className="text-lg " />
+        </div>
+        <p className="text-sm">Add shortcuts</p>
       </div>
-      {/* {isAddShortcutOpen && <AddShortcutPopUp />} */}
-      <AddShortcutPopUp />
-    </>
+      {isAddShortcutOpen && (
+        <AddShortcutPopUp changeOpen={setIsAddShortcutOpen} />
+      )}
+    </div>
   );
 }
