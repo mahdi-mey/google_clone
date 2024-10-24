9fa04db9a9d018bf8089103d24f6b67aaee9a8b6
Mahdi <mahdimey17@gmail.com>
2024-10-24 23:36:58 +0330

    add 3Dots icon on hover and placed it top right

--- src/components/Shortcuts/Shortcut.jsx
+++ src/components/Shortcuts/Shortcut.jsx
@@ -1,20 +1,22 @@
 "use client"
+import { BiDotsVerticalRounded } from "react-icons/bi"
 
-export default function Shortcut({ shrtct}) {
-  let faviconUrl;
+export default function Shortcut({ shrtct }) {
+  let faviconUrl
   try {
-    const parsedUrl = new URL(shrtct.url);
-    faviconUrl = `${parsedUrl.origin}/favicon.ico`;
+    const parsedUrl = new URL(shrtct.url)
+    faviconUrl = `${parsedUrl.origin}/favicon.ico`
   } catch (e) {
-    faviconUrl = "/images/default-web.jpg";
+    faviconUrl = "/images/default-web.jpg"
   }
+
   return (
     <a
       href={shrtct.url}
       key={shrtct.name}
       target="_blank"
       rel="noopener noreferrer"
-      className="flex size-28 cursor-pointer flex-col items-center justify-evenly rounded-md transition-all hover:bg-gray-200"
+      className="relative flex size-28 group cursor-pointer flex-col items-center justify-evenly rounded-md transition-all hover:bg-gray-200"
     >
       <div className="flex size-12 items-center justify-center rounded-full bg-gray-300/80">
         {/* eslint-disable-next-line @next/next/no-img-element */}
@@ -23,12 +25,13 @@ export default function Shortcut({ shrtct}) {
           alt={`${shrtct.name} favicon`}
           className="h-6 w-6 rounded-full"
           onError={(e) => {
-            e.target.onerror = null;
-            e.target.src = "/images/default-web.jpg";
+            e.target.onerror = null
+            e.target.src = "/images/default-web.jpg"
           }}
         />
       </div>
       <p className="text-sm">{shrtct.name}</p>
+      <BiDotsVerticalRounded className="absolute right-2 top-2 hover:bg-slate-100 rounded-full text-xl sm:opacity-0 group-hover:opacity-100 transition-all duration-500 font-bold" />
     </a>
-  );
+  )
 }
