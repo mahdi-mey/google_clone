ad930c5df4212359e9e72d10932cdbdb1ba5086d
Mahdi <mahdimey17@gmail.com>
2024-10-27 12:19:42 +0330

    add Loader when user clicks the microphone icon

--- src/components/Dictaphone.jsx
+++ src/components/Dictaphone.jsx
@@ -1,55 +1,55 @@
-// 'use client'
-// import "regenerator-runtime/runtime"
-// import SpeechRecognition, {
-//   useSpeechRecognition,
-// } from "react-speech-recognition"
-// import { useEffect, useState } from "react"
+'use client'
+import "regenerator-runtime/runtime"
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition"
+import { useEffect, useState } from "react"
 
-// const Dictaphone = () => {
-//   const {
-//     transcript,
-//     listening,
-//     resetTranscript,
-//     browserSupportsSpeechRecognition,
-//   } = useSpeechRecognition()
-//   const [isReady, setIsReady] = useState(false)
+const Dictaphone = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition()
+  const [isReady, setIsReady] = useState(false)
 
-//   useEffect(() => {
-//     if (browserSupportsSpeechRecognition) {
-//       setIsReady(true) // Set ready to true if browser supports
-//     }
-//   }, [browserSupportsSpeechRecognition])
+  useEffect(() => {
+    if (browserSupportsSpeechRecognition) {
+      setIsReady(true) // Set ready to true if browser supports
+    }
+  }, [browserSupportsSpeechRecognition])
 
-//   if (!isReady) {
-//     return (
-//       <div>
-//         <h1>Dictaphone</h1>
-//         <span>Checking for browser support...</span>
-//       </div>
-//     )
-//   }
+  if (!isReady) {
+    return (
+      <div>
+        <h1>Dictaphone</h1>
+        <span>Checking for browser support...</span>
+      </div>
+    )
+  }
 
-//   if (!browserSupportsSpeechRecognition) {
-//     return (
-//       <div>
-//         <h1>Dictaphone</h1>
-//         <span>Browser does not support speech recognition.</span>
-//       </div>
-//     )
-//   }
+  if (!browserSupportsSpeechRecognition) {
+    return (
+      <div>
+        <h1>Dictaphone</h1>
+        <span>Browser does not support speech recognition.</span>
+      </div>
+    )
+  }
 
-//   return (
-//     <div>
-//       <h1>Dictaphone</h1>
-//       <p>Microphone: {listening ? "On" : "Off"}</p>
-//       <div>
-//         <button onClick={SpeechRecognition.startListening}>Start</button>
-//         <button onClick={SpeechRecognition.stopListening}>Stop</button>
-//         <button onClick={resetTranscript}>Reset</button>
-//       </div>
-//       <p>{transcript || "Your transcriptions will appear here."}</p>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+      <h1>Dictaphone</h1>
+      <p>Microphone: {listening ? "On" : "Off"}</p>
+      <div>
+        <button onClick={SpeechRecognition.startListening}>Start</button>
+        <button onClick={SpeechRecognition.stopListening}>Stop</button>
+        <button onClick={resetTranscript}>Reset</button>
+      </div>
+      <p>{transcript || "Your transcriptions will appear here."}</p>
+    </div>
+  )
+}
 
-// export default Dictaphone
+export default Dictaphone
--- src/components/MicrophoneIcon.jsx
+++ src/components/MicrophoneIcon.jsx
@@ -1,8 +1,51 @@
-"use client";
-import { BsFillMicFill } from "react-icons/bs";
+"use client"
+import "regenerator-runtime/runtime"
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition"
+import { useEffect, useState } from "react"
+import { BsFillMicFill } from "react-icons/bs"
+import FadeLoader from 'react-spinners/FadeLoader'
+import Loader from "./Loader/Loader"
 
 export default function MicrophoneIcon() {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+    isMicrophoneAvailable
+  } = useSpeechRecognition()
+
+  const [isListening, setIslistening] = useState(false)
+
+  async function listenHandler() {
+    console.log("started listening")
+    setIslistening(true)
+    SpeechRecognition.startListening()
+
+    setTimeout(() => {
+      SpeechRecognition.stopListening
+      console.log("stopeed")
+      console.log(transcript)
+      setIslistening(false)
+    }, 5000)
+  }
+
+  if (!browserSupportsSpeechRecognition) {
+    console.log("no browser support")
+  }
+
   return (
-    <BsFillMicFill className="cursor-pointer text-lg text-gray-600 transition-all duration-200 active:scale-75" />
-  );
+    <>
+      {isListening === true ? (
+        <Loader />
+      ) : (
+        <BsFillMicFill
+          onClick={listenHandler}
+          className="cursor-pointer text-lg text-gray-600 transition-all duration-200 active:scale-75"
+        />
+      )}
+    </>
+  )
 }
