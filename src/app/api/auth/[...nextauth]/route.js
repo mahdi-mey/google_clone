47d7df235a29f88ef8a77e39b4134a7215d68d58
Mahdi <mahdimey17@gmail.com>
2024-10-28 12:54:26 +0330

    add Session provider and route.js for auth configuration

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    pages: {
+        signIn: '/auth/signin',
+    },
+    session: {
+        strategy: "jwt",
+    },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
