44e51fb59241fcabc4bb7ed61d9583f3c2ebd815
Mahdi <mahdimey17@gmail.com>
2024-11-02 12:41:09 +0330

    fix Footer not sticking to the bottom

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="flex h-[100dvh] flex-col justify-between">
+      <body className="flex h-dvh flex-col justify-between">
         {children}
       </body>
     </html>
--- src/components/MainFooter.jsx
+++ src/components/MainFooter.jsx
@@ -1,11 +1,11 @@
-import Customize from "./Customize/CustomizeButton";
-import UserCountry from "./UserCountry";
+import Customize from "./Customize/CustomizeButton"
+import UserCountry from "./UserCountry"
 
 export default function MainFooter() {
   return (
-    <footer className="flex items-center justify-between bg-gray-200 py-2">
+    <footer className="absolute bottom-0 flex items-center justify-between bg-gray-200 py-2 right-0 left-0">
       <UserCountry />
       <Customize />
     </footer>
-  );
+  )
 }
