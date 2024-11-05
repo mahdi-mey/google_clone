3764a4a2c4e7331633c0672ae7b4cbd8c6cdab73
Mahdi <mahdimey17@gmail.com>
2024-11-05 10:56:22 +0330

    add AuthProvider component to be able to provide SessionProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
   title: "Google",
@@ -7,10 +8,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className="flex h-dvh flex-col justify-between">
-        {children}
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body className="flex h-dvh flex-col justify-between">
+          {children}
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
