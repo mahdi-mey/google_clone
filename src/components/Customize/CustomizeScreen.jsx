72f03e11206823d5d385fd8ba0c5de6acce3500a
Mahdi <mahdimey17@gmail.com>
2024-11-08 22:13:08 +0330

    fix closeModal function not to execute on other elements onclick

--- src/components/Customize/CustomizeScreen.jsx
+++ src/components/Customize/CustomizeScreen.jsx
@@ -1,4 +1,5 @@
-import { motion, AnimatePresence } from "framer-motion"
+import { motion } from "framer-motion"
+import ToggleSwitches from "./ToggleSwitches"
 
 export default function CustomizeScreen({ setIsCustomizeOpen }) {
   function closeModal() {
@@ -11,12 +12,13 @@ export default function CustomizeScreen({ setIsCustomizeOpen }) {
       className="absolute bottom-0 right-0 flex h-dvh w-screen justify-end bg-gray-800/35 text-white backdrop-blur-[2px]"
     >
       <motion.div
+        onClick={(e) => e.stopPropagation()}
         className="w-80 bg-white/80 text-black"
         initial={{ x: 300, opacity: 0 }}
-        animate={{ x: 0, opacity: 1 }} 
+        animate={{ x: 0, opacity: 1 }}
         exit={{ x: 300, opacity: 0 }}
       >
-        the customzie content part 
+        <ToggleSwitches />
       </motion.div>
     </motion.div>
   )
