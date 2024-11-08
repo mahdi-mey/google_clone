c94628cff46a07cae9d225a2489e71f85a26f6bf
Mahdi <mahdimey17@gmail.com>
2024-11-08 23:49:34 +0330

    add event listener for ESC ket to close modal

--- src/components/Customize/CustomizeScreen.jsx
+++ src/components/Customize/CustomizeScreen.jsx
@@ -1,22 +1,37 @@
 import { motion } from "framer-motion"
 import ToggleSwitches from "./ToggleSwitches"
+import { useEffect } from "react"
 
 export default function CustomizeScreen({ setIsCustomizeOpen }) {
   function closeModal() {
-    console.log("close modal")
     setIsCustomizeOpen(false)
   }
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === "Escape") {
+        closeModal()
+      }
+    }
+    window.addEventListener("keydown", handleKeyDown)
+
+    // Cleanup function to remove the event listener
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown)
+    }
+  }, [])
+
   return (
     <motion.div
       onClick={closeModal}
-      className="absolute bottom-0 right-0 flex h-dvh w-screen justify-end bg-gray-800/35 text-white backdrop-blur-[2px]"
+      className="absolute bottom-0 right-0 flex h-dvh w-screen justify-end text-white"
     >
       <motion.div
         onClick={(e) => e.stopPropagation()}
-        className="w-80 bg-white/80 text-black"
-        initial={{ x: 300, opacity: 0 }}
+        className="w-80 bg-gray-800/50 text-black shadow-lg backdrop-blur-3xl"
+        initial={{ x: 320, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
-        exit={{ x: 300, opacity: 0 }}
+        exit={{ x: 320, opacity: 0 }}
       >
         <ToggleSwitches />
       </motion.div>
