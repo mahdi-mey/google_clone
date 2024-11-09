bfb7734737604dc12b6bad65712ec658200a7ee0
Mahdi <mahdimey17@gmail.com>
2024-11-10 00:15:09 +0330

    fix microphone listening & used React portal to fix layout shifting of MicNotAllowd component

--- src/components/MicrophoneIcon.jsx
+++ src/components/MicrophoneIcon.jsx
@@ -1,10 +1,88 @@
+"use client"
+import "regenerator-runtime/runtime"
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition"
+import { useEffect, useState } from "react"
 import { BsFillMicFill } from "react-icons/bs"
+import Loader from "@/components/Loader/Loader"
+import { useRouter } from "next/navigation"
+import MicNotAllowed from "./Alert/MicNotAllowed"
+import { createPortal } from "react-dom"
 
 export default function MicrophoneIcon() {
+  const router = useRouter()
+  const [canSearch, setCanSearch] = useState(false)
+  const [isMicrophoneGranted, setIsMicrophoneGranted] = useState(null)
+  const [showAlert, setShowAlert] = useState(false)
+
+  function timeout(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+  } = useSpeechRecognition()
+
+  async function listenHandler() {
+    if (isMicrophoneGranted === null) {
+      await checkMicrophonePermission()
+    }
+
+    if (isMicrophoneGranted) {
+    resetTranscript()
+    console.log("started listening")
+    SpeechRecognition.startListening()
+    await timeout(4000)
+    SpeechRecognition.stopListening()
+    console.log("stopped")
+    setCanSearch(true)
+    } else {
+      console.log("you have denied access to your microphone")
+      setShowAlert(true)
+    }
+  }
+
+  const checkMicrophonePermission = async () => {
+    if (!navigator.permissions || !navigator.permissions.query) {
+      setIsMicrophoneGranted(false)
+      return
+    }
+
+    try {
+      const permissionStatus = await navigator.permissions.query({
+        name: "microphone",
+      })
+      setIsMicrophoneGranted(permissionStatus.state === "granted")
+
+      permissionStatus.onchange = () => {
+        setIsMicrophoneGranted(permissionStatus.state === "granted")
+      }
+    } catch (error) {
+      console.error("Error checking microphone permission:", error)
+      setIsMicrophoneGranted(false)
+    }
+  }
+
+  useEffect(() => {
+    if (canSearch && transcript) {
+      router.push(`/search/web?searchTerm=${transcript}`)
+    }
+  }, [transcript, canSearch, router])
+
   return (
     <>
+      {showAlert && createPortal(<MicNotAllowed />, document.body)}
+      {listening ? (
+        <Loader />
+      ) : (
         <BsFillMicFill
-          className="cursor-pointer text-lg text-gray-600 transition-all duration-200 active:scale-75" />
+          onClick={listenHandler}
+          className="cursor-pointer text-lg text-gray-600 transition-all duration-200 active:scale-75"
+        />
+      )}
     </>
   )
 }
