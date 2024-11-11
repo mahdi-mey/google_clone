3c46b4c032042d55221ef8322f2a35cf3185f2d0
Mahdi <mahdimey17@gmail.com>
2024-11-11 21:10:58 +0330

    add cross icon (x) to close customiz sidebar on small screens

--- src/components/Customize/CustomizeScreen.jsx
+++ src/components/Customize/CustomizeScreen.jsx
@@ -1,8 +1,12 @@
+import { AiOutlineClose } from "react-icons/ai"; 
 import { motion } from "framer-motion"
 import ToggleSwitches from "./ToggleSwitches"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
+import LightThemeContainer from "./LightThemeContainer"
+import DarkThemeContainer from "./DarkThemesContainer"
 
 export default function CustomizeScreen({ setIsCustomizeOpen }) {
+  const [selectedTheme, setSelectecThem] = useState("Light")
   function closeModal() {
     setIsCustomizeOpen(false)
   }
@@ -28,12 +32,25 @@ export default function CustomizeScreen({ setIsCustomizeOpen }) {
     >
       <motion.div
         onClick={(e) => e.stopPropagation()}
-        className="w-80 bg-gray-800/50 text-black shadow-lg backdrop-blur-3xl"
+        className="flex w-80 flex-col gap-3 bg-gray-800/50 p-3 px-2 text-black shadow-lg backdrop-blur-3xl"
         initial={{ x: 320, opacity: 0 }}
         animate={{ x: 0, opacity: 1 }}
         exit={{ x: 320, opacity: 0 }}
       >
-        <ToggleSwitches />
+        {/* close modal icon on small screens */}
+        <AiOutlineClose
+          onClick={() => setIsCustomizeOpen(false)}
+          className="text-xlg absolute bottom-7 right-8 flex size-7 cursor-pointer items-center justify-center rounded-sm bg-gray-400 p-1 text-red-500"
+        />
+        <ToggleSwitches
+          selectedTheme={selectedTheme}
+          setSelectecThem={setSelectecThem}
+        />
+        {selectedTheme === "Light" ? (
+          <LightThemeContainer />
+        ) : (
+          <DarkThemeContainer />
+        )}
       </motion.div>
     </motion.div>
   )
