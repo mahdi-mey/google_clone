b140cd58c1235c2e2dbac69fef099789d87ab2f8
Mahdi <mahdimey17@gmail.com>
2024-11-17 18:01:05 +0330

    add logic for Dark themes switching and dark styles variables

--- src/components/Customize/DarkThemesContainer.jsx
+++ src/components/Customize/DarkThemesContainer.jsx
@@ -1,9 +1,32 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./DarkPatterns.css"
+import PatternOption from "./PatternOption"
 
 export default function DarkThemeContainer() {
   // State to track the currently selected pattern
   const [selectedPattern, setSelectedPattern] = useState("default")
+  const [selectedTheme, setSelectedTheme] = useState("default")
+
+  const changeTheme = (newTheme) => {
+    document.body.classList.remove(
+      // light themes
+      "default-light",
+      "theme-red",
+      "theme-yellow",
+      "theme-green",
+      // dark themes
+      "default-dark",
+      "theme-dark-red",
+      "theme-dark-green",
+      "theme-dark-yellow",
+    )
+    if (newTheme) {
+      document.body.classList.add(newTheme)
+      setSelectedTheme(newTheme)
+    } else {
+      setSelectedTheme(null)
+    }
+  }
 
   const changePattern = (newPattern) => {
     // Remove existing pattern classes
@@ -24,34 +47,59 @@ export default function DarkThemeContainer() {
       setSelectedPattern("default") // Default selection when no pattern is selected
     }
   }
+
   return (
     <div className="flex flex-grow flex-col">
       <div className="">
         <h1 className="my-2.5 text-lg text-white">Theme</h1>
         <div className="flex flex-wrap justify-center gap-x-6 gap-y-7">
-          <div className="h-14 w-32 cursor-pointer rounded-sm bg-sky-700 shadow-current transition-all duration-200 hover:shadow-xl active:scale-90"></div>
-          <div className="h-14 w-32 cursor-pointer rounded-sm bg-sky-700 shadow-current transition-all duration-200 hover:shadow-xl active:scale-90"></div>
-          <div className="h-14 w-32 cursor-pointer rounded-sm bg-sky-700 shadow-current transition-all duration-200 hover:shadow-xl active:scale-90"></div>
-          <div className="h-14 w-32 cursor-pointer rounded-sm bg-sky-700 shadow-current transition-all duration-200 hover:shadow-xl active:scale-90"></div>
+          <PatternOption
+            from="bg-gray-800"
+            to="bg-[#298dff]"
+            fnParam="default-dark"
+            isSelected={selectedTheme === "default-dark"}
+            onSelect={changeTheme}
+          />
+          <PatternOption
+            from="bg-gray-800"
+            to="bg-[#db4437]"
+            fnParam="theme-dark-red"
+            isSelected={selectedTheme === "theme-dark-red"}
+            onSelect={changeTheme}
+          />
+          <PatternOption
+            from="bg-gray-800"
+            to="bg-[#0f9d58]"
+            fnParam="theme-dark-green"
+            isSelected={selectedTheme === "theme-dark-green"}
+            onSelect={changeTheme}
+          />
+          <PatternOption
+            from="bg-gray-800"
+            to="bg-[#f4b400]"
+            fnParam="theme-dark-yellow"
+            isSelected={selectedTheme === "theme-dark-yellow"}
+            onSelect={changeTheme}
+          />
         </div>
       </div>
       <div className="mt-5">
         <h1 className="my-2.5 text-lg text-white">Background Image</h1>
         <div className="flex flex-wrap justify-center gap-x-6 gap-y-7">
           <div
-            onClick={() => changePattern(null)} // Change pattern to null for default
-            className={`flex h-14 w-32 cursor-pointer items-center justify-center rounded-sm bg-white text-center shadow-current transition-all duration-200 hover:shadow-xl active:scale-90 ${selectedPattern === "default" ? "selected" : ""}`}
+            onClick={() => changePattern("default")}
+            className={`flex h-14 w-32 cursor-pointer items-center justify-center rounded-sm bg-gray-800/80 text-center text-white shadow-current transition-all duration-200 hover:shadow-xl active:scale-90 ${selectedPattern === "default" ? "selected" : ""}`}
           >
             Default
           </div>
           <div
             onClick={() => changePattern("stary-night")}
-            className={`stary-night h-14 w-32 cursor-pointer rounded-sm bg-rose-700 shadow-current transition-all duration-200 hover:shadow-xl active:scale-90 ${selectedPattern === "stary-night" ? "selected" : ""}`}
+            className={`stary-night-for-div h-14 w-32 cursor-pointer rounded-sm bg-rose-700 shadow-current transition-all duration-200 hover:shadow-xl active:scale-90 ${selectedPattern === "stary-night" ? "selected" : ""}`}
           ></div>
           <div
             onClick={() => changePattern("threeD-boxes")}
             className={`threeD-boxes h-14 w-32 cursor-pointer rounded-sm bg-rose-700 shadow-current transition-all duration-200 hover:shadow-xl active:scale-90 ${selectedPattern === "threeD-boxes" ? "selected" : ""}`}
-            ></div>
+          ></div>
           <div
             onClick={() => changePattern("stairs")}
             className={`stairs h-14 w-32 cursor-pointer rounded-sm bg-rose-700 shadow-current transition-all duration-200 hover:shadow-xl active:scale-90 ${selectedPattern === "stairs" ? "selected" : ""}`}
--- src/components/Customize/LightThemeContainer.jsx
+++ src/components/Customize/LightThemeContainer.jsx
@@ -1,38 +1,26 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import "./LightPatterns.css"
 import PatternOption from "./PatternOption"
+import { changeTheme } from "./changeThemeFn"
+import { changePattern } from "./changePatternFn"
 
 export default function LightThemeContainer() {
   const [selectedPattern, setSelectedPattern] = useState("default")
   const [selectedTheme, setSelectedTheme] = useState("default")
 
-  const changePattern = (newPattern) => {
-    // Remove existing pattern classes
-    document.body.classList.remove(
-      "dots",
-      "zig-zag",
-      "paper",
-      "stary-night",
-      "threeD-boxes",
-      "stairs",
-    )
-
-    // Add the new pattern class if not null
-    if (newPattern) {
-      document.body.classList.add(newPattern)
-      setSelectedPattern(newPattern)
-    } else {
-      setSelectedPattern("default")
-    }
-  }
-
   const changeTheme = (newTheme) => {
     // Remove existing theme classes
     document.body.classList.remove(
-      "default",
+      // light themes
+      "default-light",
       "theme-red",
       "theme-yellow",
       "theme-green",
+      // dark themes
+      "default-dark",
+      "theme-dark-red",
+      "theme-dark-green",
+      "theme-dark-yellow",
     )
     if (newTheme) {
       document.body.classList.add(newTheme)
@@ -42,37 +30,57 @@ export default function LightThemeContainer() {
     }
   }
 
+   const changePattern = (newPattern) => {
+     // Remove existing pattern classes
+     document.body.classList.remove(
+       "dots",
+       "zig-zag",
+       "paper",
+       "stary-night",
+       "threeD-boxes",
+       "stairs",
+     )
+
+     // Add the new pattern class if not null
+     if (newPattern) {
+       document.body.classList.add(newPattern)
+       setSelectedPattern(newPattern)
+     } else {
+       setSelectedPattern("default")
+     }
+   }
+
   return (
     <div className="flex flex-grow flex-col">
       <div>
         <h1 className="my-2.5 text-lg text-white">Themes</h1>
         <div className="flex flex-wrap justify-center gap-x-6 gap-y-7">
           <PatternOption
-            from="bg-muted"
+            from="bg-gray-200"
             to="bg-[#298dff]"
-            fnParam="default"
-            isSelected={selectedTheme === "default"}
+            fnParam="default-light"
+            isSelected={selectedTheme === "default-light"}
             onSelect={changeTheme}
           />
           <PatternOption
-            from="bg-muted"
+            from="bg-gray-200"
             to="bg-[#db4437]"
             fnParam="theme-red"
             isSelected={selectedTheme === "theme-red"}
             onSelect={changeTheme}
           />
           <PatternOption
-            from="bg-muted"
-            to="bg-[#f4b400]"
-            fnParam="theme-yellow"
-            isSelected={selectedTheme === "theme-yellow"}
-            onSelect={changeTheme}
-          />
-          <PatternOption
-            from="bg-muted"
+            from="bg-gray-200"
             to="bg-[#0f9d58]"
             fnParam="theme-green"
             isSelected={selectedTheme === "theme-green"}
+            onSelect={changeTheme}
+          />
+          <PatternOption
+            from="bg-gray-200"
+            to="bg-[#f4b400]"
+            fnParam="theme-yellow"
+            isSelected={selectedTheme === "theme-yellow"}
             onSelect={changeTheme}
           />
         </div>
@@ -81,7 +89,7 @@ export default function LightThemeContainer() {
         <h1 className="my-2.5 text-lg text-white">Background Images</h1>
         <div className="flex flex-wrap justify-center gap-x-6 gap-y-7">
           <div
-            onClick={() => changePattern("default")}
+            onClick={() => changePattern("default-light")}
             className={`flex h-14 w-32 cursor-pointer items-center justify-center rounded-sm bg-white text-center shadow-current transition-all duration-200 hover:shadow-xl active:scale-90 ${selectedPattern === "default" ? "selected" : ""}`}
           >
             Default
