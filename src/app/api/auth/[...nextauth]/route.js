e43c2a3dcf126571ab8a81bc56c7c369ce90240b
Mahdi <mahdimey17@gmail.com>
2024-11-25 16:41:22 +0330

    add comunity best practices to route.js

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,15 +6,15 @@ const handler = NextAuth({
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    prompt: 'consent',
+                    access_type: 'offline',
+                    response_type: 'code'
+                }
+            }
         }),
     ],
-    authorization: {
-        params: {
-            prompt: 'consent',
-            access_type: 'offline',
-            response_type: 'code'
-        }
-    },
 })
 
 export { handler as GET, handler as POST }
